4f8de0877873b0f1c0a1bcdf3a4ff70a68ae0370
AleBera03 <alebestpc03@gmail.com>
2026-03-30 21:44:33 +0200

    added ssh key generation

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+REMOTE_IP="192.168.2.99"
+
 # Build essentials
 sudo apt update -y && sudo apt upgrade -y && sudo apt install build-essential -y
+
+# Check ssh configuration
+if [[ -z $(ls -l ~/.ssh/id_*) ]]; then
+    ssh-keygen -t rsa
+fi
+if [[ -z $(ssh xilinx@$REMOTE_IP "grep '$(cat ~/.ssh/id_*.pub)' ~/.ssh/authorized_keys") ]]; then
+    ssh-copy-id xilinx@$REMOTE_IP
+fi
 
 # Capture the starting directory
 ORIGINAL_DIR=$(pwd)
 INSTALL_DIR="/opt/pz2"
 
-# 1. Download/Update repository
+# Download/Update repository
 if [ -d "pynq-proj-gen" ]; then
     echo "Directory exists, pulling latest changes..."
     cd pynq-proj-gen && git pull origin master
@@ -16,24 +26,24 @@ else
     cd pynq-proj-gen
 fi
 
-# 2. Build the Rust project
+# Build the Rust project
 echo "Building project with Cargo..."
 cargo build -r
 
-# 3. Setup /opt/pz2 structure
+# Setup /opt/pz2 structure
 echo "Setting up $INSTALL_DIR..."
 sudo rm -rf "$INSTALL_DIR"
 sudo mkdir -p "$INSTALL_DIR/bin"
 sudo mkdir -p "$INSTALL_DIR/.scripts"
 
-# 4. Copy bin file and scripts
+# Copy bin file and scripts
 sudo cp ./target/release/pz2 "$INSTALL_DIR/bin/" -v
 sudo cp -r ./.scripts/* "$INSTALL_DIR/.scripts/" -v
 
-# 5. Set permissions
+# Set permissions
 sudo chmod -R 755 "$INSTALL_DIR"
 
-# 6. Add to PATH safely (only if not already there)
+# Add to PATH safely (only if not already there)
 PATH_LINE="export PATH=\$PATH:$INSTALL_DIR/bin"
 if ! grep -qF "$PATH_LINE" ~/.bashrc; then
     echo "Adding $INSTALL_DIR/bin to PATH in .bashrc"
