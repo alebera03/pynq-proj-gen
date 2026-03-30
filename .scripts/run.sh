11a4a5d7f3aea41473324396f1dd96ca0bd773f5
AleBera03 <alebestpc03@gmail.com>
2026-03-30 21:24:20 +0200

    inserted defualt ip

--- .scripts/run.sh
+++ .scripts/run.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+# ./main.py because run-remote.sh use it directly in workdir ./ and not ./.scripts
 file="./main.py"
 if [[ -n $1 ]]; then
     file=$1
 fi
 
+CURRENT_DIR=$(pwd)
+cd "$CURRENT_DIR/.scripts"
+
 # save envs
 set -a
-source .env
+source ./.env
 set +a
 
 # check if dir already exists
-ssh xilinx@169.254.91.19 "mkdir -p $REMOTE_PROJECT_PATH"
+ssh xilinx@192.168.2.99 "mkdir -p $REMOTE_PROJECT_PATH"
 
 # sync files
-rsync -avz --delete --exclude .env \
-            --exclude run-remote.sh \
-            --exclude run.sh \
-            ./ xilinx@169.254.91.19:$REMOTE_PROJECT_PATH
+rsync -avz --delete --exclude ./.scripts \
+            .. xilinx@192.168.2.99:$REMOTE_PROJECT_PATH
 
 # run pseudo-terminal shell attaching run-remote.sh
-ssh -t xilinx@169.254.91.19 "cd $REMOTE_PROJECT_PATH && bash -c '$(cat run-remote.sh)' -- $file"
+ssh -t xilinx@192.168.2.99 "cd $REMOTE_PROJECT_PATH && bash -c '$(cat run-remote.sh)' -- $file"
+
+cd $CURRENT_DIR
