3f0a69df5cd94457efc65f991c8cc63afacf9bc3
AleBera03 <alebestpc03@gmail.com>
2026-04-10 00:12:24 +0200

    fixed for folder with spaces

--- .utils/open.sh
+++ .utils/open.sh
@@ -4,8 +4,8 @@
 
 # save envs
 set -a
-source $1
+source "$1"
 set +a
 echo "envs saved"
 
-exec ssh -t -p $REMOTE_PORT xilinx@$REMOTE_IP "cd $REMOTE_PROJECT_PATH && bash --login"
+exec ssh -t -p "$REMOTE_PORT" "xilinx@$REMOTE_IP" "cd $REMOTE_PROJECT_PATH && bash --login"
--- .utils/sync.sh
+++ .utils/sync.sh
@@ -4,17 +4,17 @@
 
 # save envs
 set -a
-source $1
+source "$1"
 set +a
 echo "envs saved"
 
 # check if dir already exists
-ssh -p $REMOTE_PORT xilinx@$REMOTE_IP "mkdir -p $REMOTE_PROJECT_PATH"
+ssh -p "$REMOTE_PORT" "xilinx@$REMOTE_IP" "mkdir -p $REMOTE_PROJECT_PATH"
 echo "remote folder: $REMOTE_PROJECT_PATH is ready"
 
 # sync files
-git add $LOCAL_PROJECT_PATH
+git add "$LOCAL_PROJECT_PATH"
 rsync -avz -h -P \
     --exclude='.git' \
     --filter=':- .gitignore' -e "ssh -p $REMOTE_PORT" \
-    $LOCAL_PROJECT_PATH xilinx@$REMOTE_IP:$REMOTE_PROJECT_PATH
+    "$LOCAL_PROJECT_PATH/" "xilinx@$REMOTE_IP:$REMOTE_PROJECT_PATH"
